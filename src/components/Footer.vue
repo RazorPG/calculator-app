c0e42b3fbd87a3ac790e2e183a7faa738e653166
RazorPG <rafidhilmi0504@gmail.com>
2024-08-08 22:36:25 +0700

    memperbaiki ukuran dari footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer
-    class="absolute bottom-0 w-full h-12 text-white bg-color-primary flex justify-center items-center"
+    class="absolute bottom-0 w-full py-2 text-white bg-color-primary flex justify-center items-center text-xs md:text-sm lg:text-lg"
   >
     Copyright © 2024
     <a href="https://www.instagram.com/rfd_hlmi08" class="font-bold mx-1">
